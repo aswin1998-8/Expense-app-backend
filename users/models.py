d6950ecb7cddbe29058601b82d870fc62f7b0c07
Testuser <testuser@Datas-MacBook-Pro.local>
2026-05-11 11:09:35 +0530

    Initial users table migration and updated git ignore

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models  import AbstractUser
 
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = []
 # Create your models here.
